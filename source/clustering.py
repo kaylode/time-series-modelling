3cdebcabd8d795e4c2fcb8bcaf7d81b4703f0429
kaylode <kayp.kieran@gmail.com>
2023-12-09 11:04:58 +0000

    :ambulance: Improve clustering

--- source/clustering.py
+++ source/clustering.py
@@ -4,17 +4,17 @@ import os.path as osp
 import yaml
 import argparse
 import matplotlib.pyplot as plt
-import math
+import numpy as np
+import joblib
 
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
 
-from kneed import KneeLocator
 from tsfresh import extract_features
 from tslearn.clustering import TimeSeriesKMeans
 from tslearn.preprocessing import TimeSeriesScalerMinMax
 from source.constants import TASK_COLUMNS
+from source.visualize import visualize_pca, visualize_series_cluster, visualize_elbow
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default='data')
@@ -34,53 +34,64 @@ def merge_files(data_dir):
     result_df = pd.concat(final_df, axis=0)
     return result_df
 
-def feature_engineering(df, time_column, value_column, out_dir=None):
+def feature_engineering(df, time_column, value_column, out_dir=None, method:str='norm'):
     os.makedirs(out_dir, exist_ok=True)
-    # Extract features
-    features = extract_features(
-        df, 
-        column_id='id', 
-        column_sort=time_column, 
-        column_value=value_column
-    )
-    features = features.dropna(axis=1, how='any')
-    features.to_csv(osp.join(out_dir, 'features.csv'))
-    return features
 
+    features = None
+    id_names = []
 
-def visualize_clusters(df, time_column, value_column, out_dir=None):
-    pca = PCA(2)
-    #Transform the data
-    projected = pca.fit_transform(features)
+    # Normalize first, using min, max scale
+    unique_ids = df.id.unique()
+    for i in range(len(unique_ids)):
+        series = df[df.id == unique_ids[i]][value_column].values
+        series = TimeSeriesScalerMinMax().fit_transform(series.reshape(1, -1))
+        df.loc[df.id == unique_ids[i], value_column] = series.reshape(-1)
 
-    #Getting the Centroids
-    centroids = kmeans.cluster_centers_
+    # Extract features using tsfresh, convert from time series to feature matrix
+    if method == 'feature_matrix':
+        features = extract_features(
+            df, 
+            column_id='id', 
+            column_sort=time_column, 
+            column_value=value_column
+        )
+        features = features.dropna(axis=1, how='any')
+        features.to_csv(osp.join(out_dir, 'features.csv'))
+        id_names = features.index.values
 
-    for i, pred in enumerate(predictions):
-        plt.scatter(projected[i, 0], projected[i, 1], label=pred)
-    plt.legend()
-    plt.show()
+    # Convert from time series to feature matrix
+    elif method == 'norm':
+        features = []
+        for i in range(len(unique_ids)):
+            series = df[df.id == unique_ids[i]][value_column].values
+            features.append(series.reshape(-1))
+            id_names.append(unique_ids[i])
+    else:
+        raise ValueError('Invalid method')
+    return id_names, features
 
-def fit_kmeans(n_clusters, features, model_configs={}, method:int = 0):
-    if method == 0:
+def fit_kmeans(n_clusters, features, model_configs={}, method:str = 'kmeans'):
+    if method == 'kmeans':
         kmeans = KMeans(n_clusters=n_clusters, **model_configs)
         kmeans.fit(features)
-    elif method == 1:
-        X_normalized = TimeSeriesScalerMinMax().fit_transform(features)
+    elif method == 'tskmeans':
         kmeans = TimeSeriesKMeans(n_clusters=n_clusters, **model_configs)
-        kmeans.fit(X_normalized)
+        kmeans.fit(features)
     else:
         raise ValueError('Invalid method')
     return kmeans
 
-def search_kmeans(features, model_configs, out_dir=None, method:int = 0):
-    num_series = features.shape[0]
+def search_kmeans(features, model_configs, out_dir=None, method:str = 'kmeans'):
+    if isinstance(features, list):
+        num_series = len(features)
+    else:
+        num_series = features.shape[0]
 
     # A list holds the SSE values and silhouette score for each k
     sse = []
     silhouette_coefficients = []
 
-    for k in range(2, math.ceil(math.sqrt(num_series))+1):
+    for k in range(2, num_series):
         kmeans = fit_kmeans(k, features, model_configs, method=method)
         sse.append(kmeans.inertia_)
         score = silhouette_score(features, kmeans.labels_)
@@ -89,57 +100,51 @@ def search_kmeans(features, model_configs, out_dir=None, method:int = 0):
     optimal_k = visualize_elbow(sse, silhouette_coefficients, out_dir)
     return optimal_k
 
-def visualize_elbow(sse, silhouette_coefficients, out_dir=None):
-    num_series = len(sse) + 2
-    kl = KneeLocator(range(2, num_series), sse, curve="convex", direction="decreasing")
-    optimal_k = kl.elbow
-    import pdb; pdb.set_trace()
+def cluster(df, time_column, value_column, fe_method:str = 'norm', method:str = 'kmeans', model_configs={}, out_dir=None):
+    # Feature engineering
+    id_names, features = feature_engineering(
+        df, time_column, value_column, out_dir=out_dir,
+        method=fe_method
+    )
+
+    if method in ['kmeans', 'tskmeans']:
+        # Search for optimal k
+        optimal_k = search_kmeans(
+            features, 
+            model_configs=model_configs, 
+            out_dir=out_dir,
+            method=method
+        )
+        model = fit_kmeans(optimal_k, features, model_configs, method=method)
+        centroids = model.cluster_centers_.squeeze(-1)
     
-    plt.figure(figsize=(10, 5))
-    plt.style.use("fivethirtyeight")
-    plt.plot(range(2, num_series), sse)
-    plt.xticks(range(2, num_series))
-    plt.xlabel("Number of Clusters")
-    plt.ylabel("SSE")
-    plt.vlines(optimal_k, plt.ylim()[0], plt.ylim()[1], linestyles="--")
-    plt.title(f"Optimal k: {optimal_k}")
-    if out_dir:
-        plt.savefig(osp.join(out_dir, 'elbow.png'))
+        # Save model
+        joblib.dump(model, osp.join(out_dir, 'model.pkl'))
+    else:
+        raise ValueError('Invalid method')
 
-    plt.figure(figsize=(10, 5))
-    plt.style.use("fivethirtyeight")
-    plt.plot(range(2, num_series), silhouette_coefficients)
-    plt.xticks(range(2, num_series))
-    plt.xlabel("Number of Clusters")
-    plt.ylabel("Silhouette Coefficient")
-    if out_dir:
-        plt.savefig(osp.join(out_dir, 'silhouette.png'))
-
-    return optimal_k
-
-def cluster(df, time_column, value_column, model_configs={}, out_dir=None):
-    features = feature_engineering(
-        df, time_column, value_column, out_dir=out_dir
-    )
-
-    print("Features shape: ", features.shape)
-
-    optimal_k = search_kmeans(
-        features, 
-        model_configs=model_configs, 
+    # Make predictions
+    predictions = model.predict(features)
+    prediction_mapping = {
+        k:v for k,v in zip(id_names, predictions)
+    }
+    tmp_df = df.copy()
+    tmp_df['cluster'] = tmp_df['id'].map(prediction_mapping)
+    
+    # Visualize predictions
+    visualize_pca(
+        np.array(features), 
+        predictions, 
         out_dir=out_dir,
-        method=1
+        centroids=centroids
     )
 
-    kmeans = fit_kmeans(optimal_k, features, model_configs, method=1)
-    predictions = kmeans.predict(features)
-    print(predictions)
-    # visualize_clusters(
-    #     features, 
-    #     time_column, 
-    #     value_column, 
-    #     out_dir=out_dir
-    # )
+    # Visualize series clusters
+    tmp_df = tmp_df.set_index(time_column)
+    visualize_series_cluster(
+        tmp_df, 
+        out_dir=out_dir
+    )
 
 
 if __name__ == '__main__':
@@ -160,6 +165,8 @@ if __name__ == '__main__':
     
     cluster(
         merged_df, time_column, value_column,
+        fe_method=configs['fe_method'],
+        method=configs['cluster_method'],
         model_configs=configs['model_configs'], 
         out_dir=osp.join(OUT_DIR, 'cluster')
     )
